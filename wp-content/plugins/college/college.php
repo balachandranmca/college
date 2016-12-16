52689e015af98864655f9d769a92f4bb90aa3fc7
Bala <rbalachandranmca@gmail.com>
2016-12-16 08:46:50 +0530

    [ NEW ] Composer Update

--- wp-content/plugins/college/college.php
+++ wp-content/plugins/college/college.php
@@ -31,4 +31,24 @@ define('WP_BAG_PL_LANG_DIR', WP_BAG_PL_PATH . 'langs/');
 
 if(!isset($_SESSION)){session_start();}
 
+require_once __DIR__.'/../../../vendor/autoload.php';
+
+ 
+$capsule = new \Illuminate\Database\Capsule\Manager;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => DB_HOST,
+    'database'  => DB_NAME,
+    'username'  => DB_USER,
+    'password'  => DB_PASSWORD,
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+$capsule->setEventDispatcher(new \Illuminate\Events\Dispatcher(new \Illuminate\Container\Container));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
 include_once 'plugin_loader.php';
--- wp-content/plugins/college/shortcode/college_demo.php
+++ wp-content/plugins/college/shortcode/college_demo.php
@@ -4,8 +4,11 @@
  * eg: [COLLEGE_DEMO]
  */
 
+use App\Journal;
+
 add_shortcode('COLLEGE_DEMO', 'college_demo_shortcode');
-function college_demo_shortcode($atts) {	
+function college_demo_shortcode($atts) {
+	echo '<pre>';print_r(Journal::all());die;	
 	ob_start();
 	include_once 'template/college_demopage.php';
 	$template_content = ob_get_contents();
