fe1727d3e785326a417a277e843f96cbc0c20aaf
Nivesh <niveashkumar@gmail.com>
2016-12-28 23:36:53 +0530

    [ NEW ] - Home Slider List UI Changes

--- wp-content/plugins/college/shortcode/template/college_about_uspage.php
+++ wp-content/plugins/college/shortcode/template/college_about_uspage.php
@@ -1,1 +1,12 @@
-Nivesh Kumar
+<div>
+    <div class="col-md-6">
+        <img class="slider-list-img" src="../images/rsz_1book1.png" alt="List-img">
+    </div>
+     <div class="col-md-6">
+        <h4>International Journal of Research in Engineering, Science and Technologies (IJRESTs)</h4>
+        <p>The International Journal of Research in Engineering, Science and Technologies (IJRESTs) is an online journal in English published monthly. This academic journal is an open access, peer reviewed and fully refereed international journal. We focus on Engineering, Science, Humanities, Technology and Management related topics. Papers will be published only after thorough review by the reviewers. All our journals will cover papers on review, case studies, research, survey, etc.</p>
+        <br>
+        <br>
+        <p>IJRESTs expects that academicians, researchers, scholars, industrialists, practicing engineers, managers, etc. would make use of this journal publication for the development of Engineering, Sciences, Humanities, Mangement Studies and Technologies.</p>
+    </div>
+</div>
