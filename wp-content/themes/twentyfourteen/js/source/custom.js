cfb49c6f14228a04db59de658eca168fc5adfbe1
Nivesh <niveashkumar@gmail.com>
2017-02-01 07:44:30 +0530

    [ NEW ] - Home Page Carousel Implementation

--- wp-content/themes/twentyfourteen/js/source/custom.js
+++ wp-content/themes/twentyfourteen/js/source/custom.js
@@ -187,18 +187,18 @@ $(window).bind("load", function () {
 
 
 jQuery(function ($) {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1500);
-        return false;
-      }
-    }
-  });
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 1500);
+//         return false;
+//       }
+//     }
+//   });
 });
 
 
@@ -264,4 +264,8 @@ jQuery(function ($) {
 	$('#outer').mouseleave(function() { start(); })
 		start();
 	});
+	
+	 $('#Carousel').carousel({
+        interval: 4000
+    });
 });
