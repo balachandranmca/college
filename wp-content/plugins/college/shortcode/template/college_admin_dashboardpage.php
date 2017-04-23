7ad0ccb85d04467572fa720f5a74b7c9f58646ce
Nivesh <niveashkumar@gmail.com>
2017-04-23 16:54:09 +0530

    [ NEW ]  Textarea resize and & New Review Box added both admin dashboard and admin paper status page

--- wp-content/plugins/college/shortcode/template/college_admin_dashboardpage.php
+++ wp-content/plugins/college/shortcode/template/college_admin_dashboardpage.php
@@ -28,7 +28,7 @@
 </section>-->
 <div class="container admin-dashboard-block">
     <div class="row admin-dashboard">
-        <div class="col-xs-12 col-md-12">
+        <div class="col-md-offset-1 col-xs-12 col-md-12">
             <div class="col-xs-12 col-sm-3 col-md-3">
                 <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=recieved" class="btn btn-default btn-lg" role="button"><span><?php echo $accept;?></span> <br/>Newly Recieved Paper</a>
             </div>
@@ -38,22 +38,28 @@
             <div class="col-xs-12 col-sm-3 col-md-3">
                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=reject" class="btn btn-danger btn-lg" role="button"><span><?php echo $reject;?></span> <br/> Rejected Paper</a>
             </div>
+        </div>
+        <div class="col-md-offset-1 col-xs-12 col-md-12">
             <div class="col-xs-12 col-sm-3 col-md-3">
                 <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=modify" class="btn btn-info btn-lg" role="button"><span><?php echo $modify;?></span> <br/>Modified Paper</a>
             </div>
-        </div>
-        <div class="col-xs-12 col-md-12">
             <div class="col-xs-12 col-sm-3 col-md-3">
                 <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=resubmitted" class="btn btn-warning btn-lg" role="button"><span><?php echo $resubmitted;?></span> <br/>Resubmitted Paper</a>
             </div>
             <div class="col-xs-12 col-sm-3 col-md-3">
                 <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=transactionSubmitted" class="btn btn-primary btn-lg" role="button"><span><?php echo $transactionSubmitted;?></span> <br/>Transaction Submitted Paper</a>
             </div>
+            
+        </div>
+        <div class="col-md-offset-1 col-xs-12 col-md-12">
             <div class="col-xs-12 col-sm-3 col-md-3">
                 <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=paid" class="btn btn-magick btn-lg" role="button"><span><?php echo $paid;?></span> <br/>Paid Paper</a>
             </div>
             <div class="col-xs-12 col-sm-3 col-md-3">
                 <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=published" class="btn btn-pressure btn-lg" role="button"><span><?php echo $published;?></span> <br/>Published Paper</a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=published" class="btn btn-review btn-lg" role="button"><span><?php echo $published;?></span> <br/>Published Paper</a>
             </div>
         </div>
     </div>
