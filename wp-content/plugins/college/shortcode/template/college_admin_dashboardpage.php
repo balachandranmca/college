4e19879788c8d670807582e73763bd6046fa82c2
Nivesh <niveashkumar@gmail.com>
2017-04-23 12:45:17 +0530

    [ UPDATE ] - Admin/Reviewer Dashboard Design Changes

--- wp-content/plugins/college/shortcode/template/college_admin_dashboardpage.php
+++ wp-content/plugins/college/shortcode/template/college_admin_dashboardpage.php
@@ -1,4 +1,4 @@
-<section class="aboutus-page">
+<!--<section class="admin-dashboard aboutus-page">
     <div class="row">
         <div class="col-md-6">
             <div><a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=recieved"><?php echo $recieved;?> Newly Recieved Paper </a></div>
@@ -25,4 +25,36 @@
             <div><div><a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=published"><?php echo $published;?> Published Paper </a></div></div>
         </div>
     </div>
-</section>
+</section>-->
+<div class="container admin-dashboard-block">
+    <div class="row admin-dashboard">
+        <div class="col-xs-12 col-md-12">
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=recieved" class="btn btn-default btn-lg" role="button"><span><?php echo $accept;?></span> <br/>Newly Recieved Paper</a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=accept" class="btn btn-primary btn-lg" role="button"><span><?php echo $accept;?></span> <br/>Accepted Paper</a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+               <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=reject" class="btn btn-success btn-lg" role="button"><span><?php echo $reject;?></span> <br/> Rejected Paper</a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=modify" class="btn btn-info btn-lg" role="button"><span><?php echo $modify;?></span> <br/>Modified Paper</a>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-12">
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=resubmitted" class="btn btn-warning btn-lg" role="button"><span><?php echo $resubmitted;?></span> <br/>Resubmitted Paper</a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=transactionSubmitted" class="btn btn-danger btn-lg" role="button"><span><?php echo $transactionSubmitted;?></span> <br/>Transaction Submitted Paper</a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=paid" class="btn btn-magick btn-lg" role="button"><span><?php echo $paid;?></span> <br/>Paid Paper</a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=published" class="btn btn-pressure btn-lg" role="button"><span><?php echo $published;?></span> <br/>Published Paper</a>
+            </div>
+        </div>
+    </div>
+</div>
--- wp-content/plugins/college/shortcode/template/college_reviewer_dashboardpage.php
+++ wp-content/plugins/college/shortcode/template/college_reviewer_dashboardpage.php
@@ -1,4 +1,4 @@
-<section class="aboutus-page">
+<!--<section class="aboutus-page">
     <div class="row">
         <div class="col-md-6">
             <div><a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=assigned"><?php echo $assigned;?> Newly Assigned </a></div>
@@ -7,4 +7,17 @@
             <div><div><a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=reviewed"><?php echo $reviewed;?> Already Reviewed </a></div></div>
         </div>
     </div>
-</section>
+</section>-->
+
+<div class="container reviewer-dashboardblock">
+    <div class="row admin-dashboard">
+        <div class="col-xs-12 col-md-12">
+            <div class="col-xs-12 col-sm-3 col-md-3 text-center">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=assigned" class="btn btn-default btn-lg" role="button"><span><?php echo $assigned;?></span> <br/>Newly Assigned</a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 text-center">
+                <a href="<?php echo get_buzz_url('college_author_paper_list')?>?key=reviewed" class="btn btn-warning btn-lg" role="button"><span><?php echo $reviewed;?></span> <br/>Already Reviewed</a>
+            </div>
+        </div>
+    </div>
+</div>
