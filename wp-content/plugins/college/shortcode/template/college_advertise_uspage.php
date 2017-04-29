796742decf6cfa976da120e71e7bb8e89eabf16e
Nivesh <niveashkumar@gmail.com>
2017-04-29 09:20:38 +0530

    [ NEW ] - Add Image Upload Button in Advertise us  Form

--- wp-content/plugins/college/shortcode/template/college_advertise_uspage.php
+++ wp-content/plugins/college/shortcode/template/college_advertise_uspage.php
@@ -1,6 +1,6 @@
 <!-- Srart Contact Us
 =========================================== -->		
-<section class="contact_us_page">
+<section class="contact_us_page advertise_page">
     <div class="container">
         <div class="row">
             
@@ -24,6 +24,24 @@
                     
                     <div class="form-group">
                         <input type="email" placeholder="Email Address" class="form-control" name="email" id="email">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="input-group image-preview">
+                            <input type="text" class="form-control image-preview-filename" disabled="disabled"> <!-- don't give a name === doesn't send on POST/GET -->
+                            <span class="input-group-btn">
+                                <!-- image-preview-clear button -->
+                                <button type="button" class="btn btn-default image-preview-clear" style="display:none;">
+                                    <span class="glyphicon glyphicon-remove"></span> Clear
+                                </button>
+                                <!-- image-preview-input -->
+                                <div class="btn btn-default image-preview-input">
+                                    <span class="glyphicon glyphicon-folder-open"></span>
+                                    <span class="image-preview-input-title">Browse</span>
+                                    <input type="file" accept="image/png, image/jpeg, image/gif" name="input-file-preview"/> <!-- rename it -->
+                                </div>
+                            </span>
+                        </div><!-- /input-group image-preview [TO HERE]--> 
                     </div>
                     
                     <div class="form-group">
