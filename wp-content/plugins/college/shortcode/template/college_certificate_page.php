2aa2a5e5692db3481dd020bf315d2874fd2e64ee
Nivesh <niveashkumar@gmail.com>
2017-05-24 00:39:16 +0530

    [ NEW ] - Certificate page inline style added

--- wp-content/plugins/college/shortcode/template/college_certificate_page.php
+++ wp-content/plugins/college/shortcode/template/college_certificate_page.php
@@ -1,33 +1,34 @@
-<section class="certificate-header">
+<section style="background-image: url(../wp-content/themes/twentyfourteen/img/certificate.png);
+margin: 25px 60px 0;background-size: cover;height: 821px;">
     <div class="container">
-        <div class="row certificate-block">
+        <div class="row certificate-block" style="margin: 126px 64px;">
             <div class="col-xs-12 col-sm-12 col-md-12 header" style="background: #;">
-                    <div class="col-xs-12 col-sm-2 col-md-12 header-img">
-                        <div class="img-block">
-                            <img src="<?php echo site_url();?>/wp-content/themes/twentyfourteen/img/header-logo.png"/>
+                    <div class="col-xs-12 col-sm-2 col-md-12 header-img" style="text-align: center;">
+                        <div class="img-block" style="display: inline-block;">
+                            <img width="100" height="100" src="<?php echo site_url();?>/wp-content/themes/twentyfourteen/img/header-logo.png"/>
                         </div>
                         <!--<div class="issn-no">
                              <p style="color: #;">ISSN 2395-6453(Online)</p>
                         </div>-->
                     </div>
-                    <div class="col-xs-12 col-sm-7 col-md-12 header-title">
-                        <p class="sub-title">Deepam Publication's</p>
-                        <h2>International Jounernal of Research in Engineering,
+                    <div class="col-xs-12 col-sm-7 col-md-12 header-title" style="text-align: center;">
+                        <p class="sub-title" style="font-family: cursive;font-size: 22px;color: #ffa500;font-weight: bold;padding: 0 !important;">Deepam Publication's</p>
+                        <h2 style="margin: 0;font-family: fantasy;color: #333;">International Jounernal of Research in Engineering,
                         Science and </br>Technologies (IJRESTs)</h2>
                     </div>
                     <div class="col-xs-12 col-sm-7 col-md-12 certificate-title">
-                        <h2 class="sub-title">Certificate of Publication</h2>
+                        <h2 class="sub-title" style="text-align: center;color: #a94442;">Certificate of Publication</h2>
                     </div>
                     <div class="col-xs-12 col-sm-7 col-md-12 certificate-content">
-                        <p class="contents">This is to certify that Dr./Mr./Mrs.  ....................................... 
+                        <p class="contents" style="line-height: 40px;font-family: monospace !important;font-size: 20px;">This is to certify that Dr./Mr./Mrs.  ....................................... 
                         authored a paper entitled.................................................................been published in volume 1, 
                         No. 1, March 2015 of International Journal of Research in Engineering, Science and Technologies(IJRESTs)</p>
                     </div>
                     <div class="col-xs-12 col-sm-7 col-md-12 footer-content">
-                        <div class="address">
+                        <div class="address" style="float: left;color: #2196F3;font-family: cursive;">
                             <p>Website : www.ijrests.org</p>
                         </div>
-                        <div class="author">
+                        <div class="author" style="float: right;">
                             <h4>Dr. Y R M Rao, B.E(Civil), M.E, Ph.D.,</h4>
                             <h5>Editor-in-Chief, IJERSTs</h5>
                             <h6>Email : editor@ijrests.org</h6>
