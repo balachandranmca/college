b93a42432b079ab7a46165e1848d8c9dfb915b73
Bala <rbalachandranmca@gmail.com>
2017-06-17 15:44:23 +0530

    [ NEW ] Certificate PDF

--- wp-content/plugins/college/shortcode/template/college_certificate_page.php
+++ wp-content/plugins/college/shortcode/template/college_certificate_page.php
@@ -1,41 +1,79 @@
-<section style="background-image: url(../wp-content/themes/twentyfourteen/img/certificate.png);
-margin: 25px 60px 0;background-size: cover;height: 821px;">
-    <div class="container">
-        <div class="row certificate-block" style="margin: 126px 64px;">
-            <div class="col-xs-12 col-sm-12 col-md-12 header" style="background: #;">
-                    <div class="col-xs-12 col-sm-2 col-md-12 header-img" style="text-align: center;">
-                        <div class="img-block" style="display: inline-block;">
-                            <img width="100" height="100" src="<?php echo site_url();?>/wp-content/themes/twentyfourteen/img/header-logo.png"/>
-                        </div>
-                        <!--<div class="issn-no">
-                             <p style="color: #;">ISSN 2395-6453(Online)</p>
-                        </div>-->
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-12 header-title" style="text-align: center;">
-                        <p class="sub-title" style="font-family: cursive;font-size: 22px;color: #ffa500;font-weight: bold;padding: 0 !important;">Deepam Publication's</p>
-                        <h2 style="margin: 0;font-family: fantasy;color: #333;">International Jounernal of Research in Engineering,
-                        Science and </br>Technologies (IJRESTs)</h2>
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-12 certificate-title">
-                        <h2 class="sub-title" style="text-align: center;color: #a94442;">Certificate of Publication</h2>
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-12 certificate-content">
-                        <p class="contents" style="line-height: 40px;font-family: monospace !important;font-size: 20px;">This is to certify that Dr./Mr./Mrs.  ....................................... 
-                        authored a paper entitled.................................................................been published in volume 1, 
-                        No. 1, March 2015 of International Journal of Research in Engineering, Science and Technologies(IJRESTs)</p>
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-12 footer-content">
-                        <div class="address" style="float: left;color: #2196F3;font-family: cursive;">
-                            <p>Website : www.ijrests.org</p>
-                        </div>
-                        <div class="author" style="float: right;">
-                            <h4>Dr. Y R M Rao, B.E(Civil), M.E, Ph.D.,</h4>
-                            <h5>Editor-in-Chief, IJERSTs</h5>
-                            <h6>Email : editor@ijrests.org</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> 
-</section>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+      @import url(http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800);
+      [style*="Open Sans"] {
+          font-family: Arial, sans-serif, 'Open Sans' !important
+      }
+      html,body {
+        height: 100% !important;
+      }
+      @page *{
+			    margin-top: 2.54cm;
+			    margin-bottom: 2.54cm;
+			    margin-left: 3.175cm;
+			    margin-right: 3.175cm;
+			}
+    </style>
+  </head>
+  <body style="margin: 0 50px;">
+    <!-- Begin wrapper table -->
+   <table width="7%" height="100%" cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; padding: 0; margin-left: 14px;float: left;">
+     <tr>
+       <td>
+         <img alt="sidebar" src="<?php echo WP_BAG_PL_IMAGE;?>sidebar.png" alt="headerlogo" height="630" width="100">
+       </td>
+     </tr>
+   </table>
+    <table width="83%" height="100%" cellpadding="0" cellspacing="0" border="0" style="font-family: Arial, sans-serif, 'Open Sans';border-collapse: collapse; padding: 0;background-color: #fff; font-size: 12px;">
+      <tr>
+        <td  style="text-align: center;">
+          <img style="margin-left: 150px;" src="<?php echo WP_BAG_PL_IMAGE;?>header-logo1.png" alt="headerlogo" height="130" width="130">
+          <p style="float: right;font-size: 16px;font-weight: bold;font-family: initial;">ISSN <?php echo $issn; ?>(Online)</p>
+        </td>
+      </tr>
+      <tr>
+        <td valign="top" class="container-td" align="left">
+          <table width="100%" style="margin: 20px;" cellspacing="0" cellpadding="0" border="0" align="left">
+            <tr>
+              <td><h2 style="margin: 0;font-family: fantasy;color: #494293;text-align: center;font-size: 33px;font-weight: normal;">International Jounernal of Research in Engineering,
+                        Science and </br>Technologies (IJRESTs)</h2></td>
+            </tr>
+            <tr>
+              <td><h2 style="text-align: center;color: #a94442;font-size: 28px;font-family: cursive; margin-bottom: 0;">
+                Certificate of Publication</h2>
+              </td>
+            </tr>
+          </table>
+          <table width="100%" style="margin: 0;" cellspacing="0" cellpadding="0" border="0" align="left">
+            <tr>
+              <td style="font-size: 16px; font-weight: bold;color: rgb(28, 52, 70); margin: 0;text-align: center;">
+                <p style="margin-left: 10px;text-align: left;line-height: 40px;font-family: monospace !important;font-size: 20px;">This is to certify that Dr./Mr./Mrs. <?php echo $user_name; ?> 
+                         authored a paper entitled <?php echo $issue_name; ?> been published in <?php echo $volumeName; ?>, 
+                         <?php echo $published_date; ?> of International Journal of Research in Engineering, Science and Technologies(IJRESTs)</p>  
+              </td>
+            </tr>
+          </table>
+          <table width="100%" style="margin-left: 10px;" cellspacing="0" cellpadding="0" border="0" align="left">
+            <tr>
+              <td style="text-align: left;">
+                <p style="color: #a94442;line-height: 28px;font-size: 18px;font-weight: bold;"
+                  >Website: www.ijrests.org<br>E-mail: editor@ijrests.org
+                </p>
+              </td>
+              <td style="text-align: right;float: right">
+                <p style="color: #a94442;line-height: 28px;font-size: 18px;font-weight: bold;width: 100%;text-align: center;">
+                  Dr. Y R M Rao, B.E(Civil), M.E, Ph.D.,<br>
+                  <span style="color: #474696;  ">Editor-in-Chief</span></p>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
